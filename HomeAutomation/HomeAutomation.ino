b590b914a9402ac7c9718b03e9e23b717d487455
erichstuder <richu_@hotmail.com>
2017-09-02 21:13:00 +0200

    more files added

---
+++ HomeAutomation/HomeAutomation.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		HomeAutomation.ino
+ Created:	02.09.2017 19:33:59
+ Author:	erich
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+
+}
